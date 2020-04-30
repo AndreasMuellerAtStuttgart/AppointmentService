a2a829c085e81a87333783155c0db5e88d324002
amueller <andreas.mueller@bcmsolutions.de>
2020-04-30 11:03:30 +0200

    Initial commit
    Initial commit after project setup. Closes #1

---
+++ AppointmentService/src/test/java/am/training/petclinic/AppointmentService/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package am.training.petclinic.AppointmentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
